b9829b8c0e9ad7e16be5aaf954f62866d2bd9913
pixel <edwardoliverthomas@gmail.com>
2026-02-04 17:53:54 +0000

    feat: add card class
    Added card class with dunder methods for init, repr, and eq.
    
    Added value as a property method

--- src/caboo/card.py
+++ src/caboo/card.py
@@ -1,3 +1,22 @@
 class Card:
-  def __init__(self) -> None:
-    pass
+  # Card only has the key _value
+  __slots__ = ("_value",)
+
+  def __init__(self, value: int) -> None:
+    self._value = value
+
+  def __repr__(self) -> str:
+    """printing the object returns this instead of mem address."""
+    return f"[{self.value:2}]"
+
+  def __eq__(self, value: object, /) -> bool:
+    """how the class handles comparision, the / means you can't call use the value kwarg in the func call"""
+    if not isinstance(value, Card):
+      return False
+    return self.value == value.value
+
+  @property
+  def value(self) -> int:
+    return self._value
+    
+
